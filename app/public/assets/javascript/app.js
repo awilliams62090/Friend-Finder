64912e97f11e3fd12d8b6184d76f4b1a02baee8f
Alyssa W <awilliams62090@gmail.com>
2018-10-21 18:58:56 -0600

    Adding more styling

--- app/public/assets/javascript/app.js
+++ app/public/assets/javascript/app.js
@@ -1,27 +1,32 @@
-   $(document).ready(function(){
-   
-   $("#survey").on("submit", function (event) {
-    event.preventDefault();
-    var userInput = {
-        name: $("#name").val(),
-        photo: $("#photo").val(),
-        scores: [$("#q1").val(),
-            $("#q2").val(), 
-            $("#q3").val(), 
-            $("#q4").val(), 
-            $("#q5").val(), 
-            $("#q6").val(), 
-            $("#q7").val(), 
-            $("#q8").val(), 
-            $("#q9").val(), 
-            $("#q10").val()
-        ]
-    };
-    $.post("/api/friends", userInput, function(data){
-        $("#matchName").text(data.name);
-        $("#matchPic").attr("src", data.photo);
-        $('#friendsModal').modal('toggle')
-    });
-    
-})
-});
+   $(document).ready(function () {
+
+       $("#survey").on("submit", function (event) {
+           event.preventDefault();
+           var userInput = {
+               name: $("#name").val(),
+               photo: $("#photo").val(),
+               scores: [$("#q1").val(),
+                   $("#q2").val(),
+                   $("#q3").val(),
+                   $("#q4").val(),
+                   $("#q5").val(),
+                   $("#q6").val(),
+                   $("#q7").val(),
+                   $("#q8").val(),
+                   $("#q9").val(),
+                   $("#q10").val()
+               ]
+           };
+           $.post("/api/user", userInput, function (data) {
+               $("#matchName").text(data.name);
+               $("#matchPic").attr("src", data.photo);
+               $('#friendsModal').modal('toggle')
+           });
+
+       });
+       
+       $("#playAgain").on("click", function (event) {
+           event.preventDefault();
+           $('#survey').trigger("reset");
+       })
+   });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,42 @@
 //bringing data back to user all logic fo math will go here
 
 var friends = require("../data/friends")
+var user = require("../data/user")
 
-module.exports = function(app){
-    app.get("/api/friends", function (req, res){
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     })
 
-    app.post("/api/friends", function(req, res){
-        var bestMatch= {
+    app.get("/api/user", function (req, res) {
+        res.json(user);
+    });
+
+    app.post("/api/user", function (req, res) {
+        var bestMatch = {
             name: "",
             photo: "",
             difference: 100
         };
         // send user data as an object
-        var userInput= req.body;
+        var userInput = req.body;
         var totalDiff;
 
-        for(var i=0; i<friends.length; i++){
-            totalDiff=0;
-            for(x=0; x<friends[i].scores.length; x++){
-                totalDiff += Math.abs(parseInt(userInput.scores[x])-parseInt(friends[i].scores[x]));
+        for (var i = 0; i < friends.length; i++) {
+            totalDiff = 0;
+            for (x = 0; x < friends[i].scores.length; x++) {
+                totalDiff += Math.abs(parseInt(userInput.scores[x]) - parseInt(friends[i].scores[x]));
             };
-            if(totalDiff <= bestMatch.difference){
+            if (totalDiff <= bestMatch.difference) {
                 bestMatch.name = friends[i].name;
                 bestMatch.photo = friends[i].photo;
                 bestMatch.difference = totalDiff
             }
         };
-        friends.push(userInput);
+        user.push(userInput);
+        console.log(user);
         res.json(bestMatch);
+        console.log(bestMatch)
+
     })
 }
